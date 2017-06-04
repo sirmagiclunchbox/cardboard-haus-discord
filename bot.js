eba5dcaddd6b33fed6ddc301a3e79dd68d58785d
Derek Pruitt <derek@sirmagiclunchbox.co>
2017-06-04 00:15:48 -0700

    began the configuration of the bot.js file for discord.

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
-console.log('its working');
+console.log('The bot is starting');
 
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+var config = require('config');
+bot.login('config')
