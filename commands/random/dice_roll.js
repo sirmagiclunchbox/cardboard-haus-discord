d3d1055a2c6ccf84eea145691e808d29cb70f934
Derek Pruitt <derek@sirmagiclunchbox.co>
2017-06-04 23:44:26 -0700

    Added Discord.js Commando Library and created a simple command file structure

---
+++ commands/random/dice_roll.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+
+class DiceRollCommand extends commando.Command {
+  constructor() {
+    constructor(client) {
+      super(client, {
+        name: 'roll',
+        group: 'random',
+        memberName: 'roll',
+        description: 'Rolls a die'
+      });
+    }
+  }
+
+  async run() {
+    var roll = Math.floor(Math.random() * 6) + 1;
+    message.reply("You rolled a " + roll);
+  }
+}
+
+module.exports = DiceRollCommand;
