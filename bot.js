188fc5aa48c296d7e81bbafe83073f5c99043a65
Derek Pruitt <derek@sirmagiclunchbox.co>
2017-06-14 00:24:52 -0700

    Added Stream and Audio Encoding libraries

--- bot.js
+++ bot.js
@@ -1,7 +1,43 @@
-console.log('The bot is starting');
+console.log("The bot is starting...");
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
+const TOKEN = "MzI0MDY0OTY4MTk2NDg5MjE4.DCEQpA._J1hSi3ysIoSPHmBWVh_Klk2uoQ";
+const PREFIX = "!"
 
-var config = require('config');
-bot.login('config')
+bot.on("ready", function() {
+  console.log("The bot is ready!");
+});
+
+bot.on("message", function(message) {
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0].toLowerCase()) {
+    case "ping":
+      message.channel.send("#placeholder for the ping command");
+      break;
+    case "info":
+      message.channel.send("#placeholder for info command");
+      break;
+    case "help":
+      message.channel.send("#placeholder for help command");
+      break;
+    case "music":
+      message.channel.send("#placeholder for music command");
+      break;
+    case "twitter":
+      message.channel.send("#placeholder for twiiter command");
+      break;
+    case "dice"
+      message.channel.send("#placeholder for dice roll command");
+      break;
+
+  }
+});
+
+
+
+bot.login(TOKEN);
