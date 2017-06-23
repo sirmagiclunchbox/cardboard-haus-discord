adc85f6122beec6fbdabeabf42d542edf276ce61
Derek Pruitt <derek@sirmagiclunchbox.co>
2017-06-22 23:38:22 -0700

    moved tokens and API keys to config folder

---
+++ exports/8ball.js
@@ -0,0 +1,12 @@
+module.exports = {
+  var eightBall = [
+    'It is certain',
+     'It is decidedly so',
+    'Cannot predict now',
+    'Don\'t count on it',
+     'My sources say no',
+    'Outlook not so good',
+    'You are in grave danger',
+    'Reply hazy try again'
+  ];
+}
